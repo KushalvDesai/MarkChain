6e7b8539c795a148c4ae819c3ff15f8af93f5085
shreylakhtaria <shrey.lakhataria.code@gmail.com>
2025-08-06 20:55:09 +0530

    Fixed the FE integration of Update User API

--- fe/src/gql/mutations/user.ts
+++ fe/src/gql/mutations/user.ts
@@ -1,10 +1,11 @@
 import { gql } from '@apollo/client';
 
 export const UPDATE_USER_PROFILE = gql`
-  mutation UpdateStudentProfile($input: UpdateUserProfileDto!) {
-    updateUserProfile(input: $input) {
+  mutation UpdateUserProfile($walletAddress: String!, $input: UpdateUserProfileDto!) {
+    updateUserProfile(walletAddress: $walletAddress, input: $input) {
       _id
       walletAddress
+      did
       name
       studentId
       role
